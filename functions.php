ec42ae0824fc4269a6701d588756d4fde116b089
Tom Slominski <tomslominski@gmail.com>
2016-02-18 15:38:49 +0000

    PHP 5.2 compatibility (no inline functions), improved function defaults and documentation.

--- functions.php
+++ functions.php
@@ -153,21 +153,19 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 	 *
 	 * @since 1.1
 	 * @param array $sites List of blogs from {@see ls_get_sites()}
+	 * @param string $sorting_method Data to sort by (alphabetical/date_registered/date_updated/post_count/id)
+	 * @param string $shorting_order Order to sort by (descending/ascending)
 	 */
-	function ls_sort_sites( $sites, $sorting_method, $sorting_order ) {
+	function ls_sort_sites( $sites, $sorting_method = 'id', $sorting_order = 'descending' ) {
 
 		switch ( $sorting_method ) {
 
 			case 'alphabetical':
 
 				if ( $sorting_order == 'descending' ) {
-					usort( $sites, function( $a, $b ) {
-						return strnatcasecmp( $b->blogname, $a->blogname );
-					} );
+					usort( $sites, 'ls_sort_sites_alphabetical_descending' );
 				} elseif ( $sorting_order == 'ascending' ) {
-					usort( $sites, function( $a, $b ) {
-						return strnatcasecmp( $a->blogname, $b->blogname );
-					} );
+					usort( $sites, 'ls_sort_sites_alphabetical_ascending' );
 				}
 
 				break;
@@ -175,14 +173,9 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 			case 'date_registered':
 
 				if ( $sorting_order == 'descending' ) {
-					usort( $sites, function($a, $b) {
-						return strtotime( $b->registered ) - strtotime( $a->registered );
-
-					} );
+					usort( $sites, 'ls_sort_sites_date_registered_descending' );
 				} elseif ( $sorting_order == 'ascending' ) {
-					usort( $sites, function($a, $b) {
-						return strtotime( $a->registered ) - strtotime( $b->registered );
-					} );
+					usort( $sites, 'ls_sort_sites_date_registered_ascending' );
 				}
 
 				break;
@@ -190,13 +183,9 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 			case 'date_updated':
 
 				if ( $sorting_order == 'descending' ) {
-					usort( $sites, function($a, $b) {
-						return strtotime( $b->last_updated ) - strtotime( $a->last_updated );
-					} );
+					usort( $sites, 'ls_sort_sites_date_updated_descending' );
 				} elseif ( $sorting_order == 'ascending' ) {
-					usort( $sites, function( $a, $b ) {
-						return strtotime( $a->last_updated ) - strtotime( $b->last_updated );
-					} );
+					usort( $sites, 'ls_sort_sites_date_updated_ascending' );
 				}
 
 				break;
@@ -204,13 +193,9 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 			case 'post_count':
 
 				if ( $sorting_order == 'descending' ) {
-					usort( $sites, function($a, $b) {
-						return $b->post_count - $a->post_count;
-					} );
+					usort( $sites, 'ls_sort_sites_post_count_descending' );
 				} elseif ( $sorting_order == 'ascending' ) {
-					usort( $sites, function($a, $b) {
-						return $a->post_count - $b->post_count;
-					} );
+					usort( $sites, 'ls_sort_sites_post_count_ascending' );
 				}
 
 				break;
@@ -219,13 +204,9 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 			default:
 
 				if ( $sorting_order == 'descending' ) {
-					usort( $sites, function($a, $b) {
-						return $b->blog_id - $a->blog_id;
-					} );
+					usort( $sites, 'ls_sort_sites_id_descending' );
 				} elseif ( $sorting_order == 'ascending' ) {
-					usort( $sites, function($a, $b) {
-						return $a->blog_id - $b->blog_id;
-					} );
+					usort( $sites, 'ls_sort_sites_id_ascending' );
 				}
 
 				break;
@@ -234,6 +215,126 @@ if( !function_exists( 'ls_sort_sites' ) ) {
 
 		return $sites;
 
+	}
+
+	/**
+	 * usort() function for sorting sites alphabetically in descending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_alphabetical_descending( $a, $b ) {
+		return strnatcasecmp( $b->blogname, $a->blogname );
+	}
+
+	/**
+	 * usort() function for sorting sites alphabetically in ascending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_alphabetical_ascending( $a, $b ) {
+		return strnatcasecmp( $a->blogname, $b->blogname );
+	}
+
+	/**
+	 * usort() function for sorting sites by date registered in descending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_date_registered_descending($a, $b) {
+		return strtotime( $b->registered ) - strtotime( $a->registered );
+	}
+
+	/**
+	 * usort() function for sorting sites by date registered in ascending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_date_registered_ascending($a, $b) {
+		return strtotime( $a->registered ) - strtotime( $b->registered );
+	}
+
+	/**
+	 * usort() function for sorting sites by date updated in descending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_date_updated_descending($a, $b) {
+		return strtotime( $b->last_updated ) - strtotime( $a->last_updated );
+	}
+
+	/**
+	 * usort() function for sorting sites by date updated in ascending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_date_updated_ascending( $a, $b ) {
+		return strtotime( $a->last_updated ) - strtotime( $b->last_updated );
+	}
+
+	/**
+	 * usort() function for sorting sites by post count in descending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_post_count_descending($a, $b) {
+		return $b->post_count - $a->post_count;
+	}
+
+	/**
+	 * usort() function for sorting sites by post count in ascending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_post_count_ascending($a, $b) {
+		return $a->post_count - $b->post_count;
+	}
+
+	/**
+	 * usort() function for sorting sites by site ID in descending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_id_descending($a, $b) {
+		return $b->blog_id - $a->blog_id;
+	}
+
+	/**
+	 * usort() function for sorting sites by site ID in ascending order.
+	 * To be used in {@see ls_sort_sites()}.
+	 *
+	 * @since 1.2
+	 * @param string $a First value to be compared
+	 * @param string $b Second value to be compared
+	 */
+	function ls_sort_sites_id_ascending($a, $b) {
+		return $a->blog_id - $b->blog_id;
 	}
 
 }
