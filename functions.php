ebd9ea42f682209503d4d244dd67f5b9b5f4fbb0
Tom Slominski <tomslominski@gmail.com>
2016-09-01 13:13:31 +0100

    Following WordPress' footsteps and letting the theme use system fonts rather than loading web fonts. Also removing Normalize - on such a small theme, it's not really necessary.

--- functions.php
+++ functions.php
@@ -8,13 +8,7 @@ if( !function_exists( 'ls_load_styles' ) ) {
 	 * @since 1.0
 	 */
 	function ls_load_styles() {
-
-		wp_enqueue_style( 'ls_style', get_stylesheet_uri(), array( 'normalize', 'fonts' ) );
-
-		wp_enqueue_style( 'normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' );
-
-		wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Open+Sans' );
-
+		wp_enqueue_style( 'ls_style', get_stylesheet_uri() );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'ls_load_styles' );
